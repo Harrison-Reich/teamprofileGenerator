edab0a4f55529fbe8880fcb7b88f7d44900dee0a
Harrison Reich <hreich1998@gmail.com>
2021-12-10 20:04:40 -0800

    adding functions, and inputs to app.js

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const fse = require('fs-extra');
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-const { FSEventsWatcher } = require("sane");
 
 // using inquirer to gather information about the development team members
 let teamMembers = []
@@ -19,22 +19,94 @@ const add = () => {
     {
       name: 'add'
       type: 'confirm'
-      message: 'Would you like to create another    employee profile?'
-  };
-]);
+      message: 'Would you like to create another employee profile?'
+    };
+  ]);
     .then(data => {
-      if(data.add === true) {
+      if (data.add === true) {
         createEmployee()
       } else {
         const team = render(teamMembers)
         render(teamMembers)
-        fse.outputFile('output.team.html', team, err => {
-          if (err) {console.log(err)}
+        fse.outputFile('display.team.html', team, err => {
+          if (err) { console.log(err) }
         });
       };
     });
 };
 
+const createEmployee = () => {
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your employee name?',
+    },
+    {
+      type: 'input',
+      name: 'id',
+      message: 'What is your employee identification number?',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is your employee email address?',
+    },
+    {
+      type: 'list',
+      name: 'role',
+      message: 'What is the role of your employee',
+      choices: 'Intern', 'Engineer', 'Manager'
+    }
+  ])
+    .then(answers => {
+      if (answers.role === 'Manager') {
+        inquirer.prompt([
+          {
+            type: 'input',
+            name: 'officeNumber',
+            message: 'What is the manager office number?',
+          }
+        ])
+          .then(manager1 => {
+            const manager = new Manager(answers.name, answers.id, answers.email, manager1.officeNumber)
+            teamMembers.push(manager)
+            console.log(teamMembers)
+            add()
+          })
+      } else if (answers.role === 'Engineer') {
+        inquirer.prompt([
+          {
+            type: 'input',
+            name: 'github',
+            message: 'What is the engineer github page?',
+          }
+        ])
+          .then(engineer1 => {
+            const engineer = new Engineer(answers.name, answers.id, answers.email, engineer1.github)
+            teamMembers.push(engineer)
+            console.log(teamMembers)
+            add()
+          })
+      } else if (answers.role === 'Intern') {
+        inquirer.prompt([
+          {
+            type: 'input',
+            name: 'school',
+            message: 'What is the interns school?',
+          }
+        ])
+          .then(intern1 => {
+            const intern = new Intern(answers.name, answers.id, answers.email, intern1.school)
+            teamMembers.push(intern)
+            console.log(teamMembers)
+            add()
+
+          })
+      }
+    })
+    .catch(err => console.log(err))
+}
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
